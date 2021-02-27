82a38040370c91f5cfb67a2995c8cdb38041a142
André Savioli <andre_savioli@hotmail.com>
2021-02-27 23:40:16 +0000

    Implemented a view for the Dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="min-w-0 flex-1 border-t border-gray-200 lg:flex">
+    <!-- Main Area -->
+    <section
+      class="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last"
+    >
+      <div class="overflow-hidden bg-gray-100">
+        <!-- Main Content -->
+        <!-- End Main Content -->
+      </div>
+    </section>
+    <!-- End Main Area -->
+
+    <!-- SideBar -->
+    <aside class="hidden lg:block lg:flex-shrink-0 lg:order-first">
+      <div
+        class="h-full relative flex flex-col w-56 border-r border-gray-200 bg-gray-100"
+      >
+        <!-- SideBar Content -->
+        <!-- End SideBar Content -->
+      </div>
+    </aside>
+    <!-- End SideBar -->
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  components: {}
+};
+</script>
