df1537ef1fe9e1c9d8a75d26708157dc9530a40b
André Savioli <andre_savioli@hotmail.com>
2021-02-27 23:35:02 +0000

    PageHeader component implemented

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-6">
+    <div
+      class="md:flex md:items-center md:justify-between md:space-x-5 lg:px-6"
+    >
+      <div class="flex items-center space-x-5">
+        <div>
+          <div class="text-2xl font-bold text-gray-900">Risk Form</div>
+          <span class="text-sm font-medium text-gray-500">
+            Description
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader"
+};
+</script>
