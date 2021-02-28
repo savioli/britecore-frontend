d2eb00a377431156bdc67b9c771703c4bc2ed8c0
André Savioli <andre_savioli@hotmail.com>
2021-02-28 19:11:53 +0000

    Loading of the InsuranceApplicationForm using the Risk Endpoint implemented

--- src/components/InsuranceApplicationForm.vue
+++ src/components/InsuranceApplicationForm.vue
@@ -1,7 +1,7 @@
 <template>
   <page-header
     title="Insurance Application Form"
-    description="Insurance Application Form for collecting the data related to the risk"
+    description="Insurance Application Form for collecting data related to the risk"
   ></page-header>
 
   <div v-for="riskField in riskFields" :key="riskField.id">
@@ -18,15 +18,26 @@
 <script>
 import PageHeader from "../components/PageHeader.vue";
 import TextRiskField from "../components/TextRiskField.vue";
+import RiskService from "../services/RiskService.js";
 
 export default {
   name: "InsuranceApplicationForm",
   components: { PageHeader, TextRiskField },
-  methods: {},
   data() {
     return {
+      riskId: 5,
       riskFields: []
     };
+  },
+  created() {
+    this.getRisk();
+  },
+  methods: {
+    async getRisk() {
+      RiskService.getRiskById(this.riskId).then(risk => {
+        this.riskFields = risk.risk_fields;
+      });
+    }
   }
 };
 </script>
