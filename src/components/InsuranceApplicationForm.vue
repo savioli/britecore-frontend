3e5aa28858f0d5f6114ca39be04eea9b9a0239e1
André Savioli <andre_savioli@hotmail.com>
2021-02-28 00:55:54 +0000

    Implemented partially the component InsuranceApplicationForm

---
+++ src/components/InsuranceApplicationForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <page-header
+    title="Insurance Application Form"
+    description="Insurance Application Form for collecting the data related to the risk"
+  ></page-header>
+</template>
+
+<script>
+import PageHeader from "../components/PageHeader.vue";
+
+export default {
+  name: "InsuranceApplicationForm",
+  components: { PageHeader }
+};
+</script>
