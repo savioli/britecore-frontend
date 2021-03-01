c9c6e92c331c925941efb094f024a802e94c813c
André Savioli <andre_savioli@hotmail.com>
2021-03-01 19:21:16 +0000

    Enhancements in the risk selection

--- src/components/TextRiskField.vue
+++ src/components/TextRiskField.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="md:flex">
     <div class="md:w-full">
-      <span class="flex-grow flex flex-col">
-        <span class="text-sm font-medium text-gray-900">{{ name }}</span>
+      <span class="flex-grow flex flex-col mb-1">
+        <span class="text-sm font-medium text-gray-900"
+          >{{ name }}
+          <span
+            v-if="required === true"
+            class="inline-flex items-center text-xs font-extralight text-gray-900 ml-1"
+          >
+            Required
+          </span>
+        </span>
         <span class="text-sm text-gray-500">{{ description }}</span>
       </span>
 
@@ -13,7 +21,7 @@
         placeholder=""
       />
 
-      <!-- <div class="ml-1 text-xs text-red-700">{{ errorMessage }}</div> -->
+      <div class="ml-1 text-xs text-red-700">{{ errorMessage }}</div>
     </div>
   </div>
 </template>
@@ -30,6 +38,10 @@ export default {
     description: {
       type: String,
       default: ""
+    },
+    required: {
+      type: Boolean,
+      default: false
     },
     errorMessage: {
       type: String,
