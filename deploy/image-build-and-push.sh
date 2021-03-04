8eaa6abf24e81f0a8063d5e9751b6311e40bf494
André Savioli <andre_savioli@hotmail.com>
2021-03-04 16:23:38 +0000

    Implemented a script to automate the build and the push of the Docker image

---
+++ deploy/image-build-and-push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker-compose -f docker-compose-prod.yaml build
+
+# To use the access token from your Docker CLI client:
+
+# 1. Run docker login --username <username>
+# 2. At the password prompt, enter the personal access token.
+
+docker login
+
+docker push savioli/britecore-frontend:latest
