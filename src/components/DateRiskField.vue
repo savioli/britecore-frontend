1e6a0e1e3f608a3fe2e50d12788747050a18a5ed
André Savioli <andre_savioli@hotmail.com>
2021-03-06 18:45:24 +0000

    Small adjustments in the DateRiskField component

--- src/components/DateRiskField.vue
+++ src/components/DateRiskField.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="md:flex">
-    <div class="md:w-full">
-      <span class="flex-grow flex flex-col mb-1">
-        <span class="text-sm font-medium text-gray-900"
-          >{{ name }}
-          <span
-            v-if="required === true"
-            class="inline-flex items-center text-xs font-extralight text-gray-900 ml-1"
-          >
-            Required
+  <div>
+    <div class="md:flex">
+      <div class="md:w-full">
+        <span class="flex-grow flex flex-col mb-1">
+          <span class="text-sm font-medium text-gray-900"
+            >{{ name }}
+            <span
+              v-if="required === true"
+              class="inline-flex items-center text-xs font-extralight text-gray-900 ml-1"
+            >
+              Required
+            </span>
           </span>
+          <span class="text-sm text-gray-500">{{ description }}</span>
         </span>
-        <span class="text-sm text-gray-500">{{ description }}</span>
-      </span>
 
-      <input
-        @focus="displayDatePicker"
-        @click="displayDatePicker"
-        v-model="date"
-        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 focus:outline-none focus:border-gray-300 focus:shadow-sm"
-        type="text"
-        placeholder=""
-        :readonly="true"
-      />
+        <input
+          @focus="displayDatePicker"
+          @click="displayDatePicker"
+          v-model="date"
+          class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 focus:outline-none focus:border-gray-300 focus:shadow-sm"
+          type="text"
+          placeholder=""
+          :readonly="true"
+        />
 
-      <div class="ml-1 text-xs text-red-700">{{ errorMessage }}</div>
+        <div class="ml-1 text-xs text-red-700">{{ errorMessage }}</div>
+      </div>
     </div>
-  </div>
 
-  <DatePicker
-    v-click-away="onClickAway"
-    v-if="visible"
-    class="relative z-50 mt-2"
-    :model-config="modelConfig"
-    v-model="date"
-  />
+    <DatePicker
+      v-click-away="onClickAway"
+      v-if="visible"
+      class="relative z-50 mt-2"
+      :model-config="modelConfig"
+      v-model="date"
+    />
+  </div>
 </template>
 <script>
 import { DatePicker } from "v-calendar";
