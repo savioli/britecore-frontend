4d5576e14abdd8b28d1f483945b244b3eda3374f
André Savioli <andre_savioli@hotmail.com>
2021-03-08 05:47:09 +0000

    Changed the application default title

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     port: 3000
+  },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "BriteCore FrontEnd";
+            return args;
+        })
   }
 };
